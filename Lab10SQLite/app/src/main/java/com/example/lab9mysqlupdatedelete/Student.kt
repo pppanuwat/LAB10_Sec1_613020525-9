da85c8ca5af8ac42fc91f5108502882db49b4f28
pppanuwat <panuwat.pp@kkumail.com>
2020-10-19 21:45:50 +0700

    Lab10_Sec1
    613020525-9

---
+++ Lab10SQLite/app/src/main/java/com/example/lab9mysqlupdatedelete/Student.kt
@@ -0,0 +1,7 @@
+package com.example.lab9mysqlupdatedelete
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Student(val id:String, val name: String, val age: Int){
+}
